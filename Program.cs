c10b1077d824594b8354d2711134f2c350ce8597
lucas gabriel <lucasglimaosusa12@gmail.com>
2025-03-15 03:06:16 -0300

    teste de comando

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+string nomeDaPessoa;
+string sobrenome;
+string seriefavorita;
+string genero;
+
+Console.WriteLine("Escreva o seu nome");
+
+ Console.Write("nome:"); 
+ nomeDaPessoa= Console.ReadLine()!;
+ Console.Write("sobrenome:");
+sobrenome= Console.ReadLine()!;
+
+Console.WriteLine($"Ola {nomeDaPessoa.ToUpper()} {sobrenome.ToUpper()}"!);
+Console .WriteLine();
+Console .WriteLine(@"coloque um nome de uma serie que goste ");
+
+Console .WriteLine();
+Console.ForegroundColor= ConsoleColor.Blue;
+Console.Write("Serie Favorita: ");
+Console.ResetColor();
+
+seriefavorita= Console.ReadLine()! .Replace("a","@") .Replace("e","#");
+Console.ForegroundColor= ConsoleColor.Blue;
+Console.Write("Genero: ");
+Console.ResetColor();
+
+genero= Console.ReadLine()! .Replace("a","@") .Replace("e","#");
+Console.WriteLine();
+Console.Write($"A sua serie favorita é:{seriefavorita} /e o genero é: {genero}");
